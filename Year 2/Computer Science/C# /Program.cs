76dc6ce0cd9e5de35d3c4707d3ea7a2066198e81
Marlier Thibaud <thibaud.marlier@algosup.com>
2025-05-26 10:48:06 +0200

    morning

--- Year 2/Computer Science/C# /Program.cs
+++ Year 2/Computer Science/C# /Program.cs
@@ -34,23 +34,143 @@
 //     }
 // }
 
+// static void Main(string[] args)
+// {
+//     Random random = new Random()
+//     int num = random.Next(1, 7);
+//     Console.WriteLine("Rolling a die...");
+//     Console.WriteLine("You rolled a " + num + "!");
+//     Console.ReadKey();
+
+// static void Main(string[] args)
+// {
+//     Console.WriteLine("Please enter your age: ");
+//     int age = Convert.ToInt32(Console.ReadLine())
+//     if (age >= 100)
+//     {
+//         Console.WriteLine("You are too old to sign up.");
+//     }
+//     else if (age < 0)
+//     {
+//         Console.WriteLine("Invalid age entered.");
+//     }
+//     else if (age >= 18)
+//     {
+//         Console.WriteLine("You are now signed up!")
+//     }
+//     else
+//     {
+//         Console.WriteLine("You are not old enough to sign up.");
+//     }
+//     Console.ReadKey();
+// }
+
+// static void Main(string[] args)
+// {
+//     Console.WriteLine("Please enter your name: ");
+//     string name = Console.ReadLine();
+//     if (name == "Thibaud")
+//     {
+//         Console.WriteLine("Acces granted, Thibaud!");
+//     }
+//     else if (name == "")
+//     {
+//         Console.WriteLine("You didn't enter a name.");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Access denied, " + name + "!");
+//     }
+//     Console.ReadKey();
+// }
+
+// static void Main(string[] args)
+// {
+//     Console.WriteLine("Enter a day of the week: ");
+//     string day = Console.ReadLine().ToLower()
+//     switch (day)
+//     {
+//         case "monday":
+//             Console.WriteLine("It's Monday! Have a great start to the week!");
+//             break;
+//         case "tuesday":
+//             Console.WriteLine("It's Tuesday! Keep going strong!");
+//             break;
+//         case "wednesday":
+//             Console.WriteLine("It's Wednesday! You're halfway through the week!");
+//             break;
+//         case "thursday":
+//             Console.WriteLine("It's Thursday! Almost there!");
+//             break;
+//         case "friday":
+//             Console.WriteLine("It's Friday! The weekend is almost here!");
+//             break;
+//         case "saturday":
+//             Console.WriteLine("It's Saturday! Enjoy your weekend!");
+//             break;
+//         case "sunday":
+//             Console.WriteLine("It's Sunday! Relax and recharge for the week ahead!");
+//             break;
+//         default:
+//             Console.WriteLine("That's not a valid day of the week. Please try again.");
+//             break
+//         Console.ReadKey();
+//     }
+
+// static void Main(string[] args)
+// {
+//     Console.WriteLine("Enter your name: ");
+//     string name = Console.ReadLine()
+//     while (name == "")
+//     {
+//         Console.Write("Enter your name: ");
+//         name = Console.ReadLine();
+//     }
+//     Console.WriteLine("Hello, " + name + "!");
+//     Console.ReadKey();
+// }
+
+// static void Main(string[] args)
+// {
+//     int i = 0
+//     while (i < 10)
+//     {
+//         Console.WriteLine("The value of i is: " + i);
+//         i++;
+//     }
+//     Console.WriteLine("Loop finished! The final value of i is: " + i)
+//     for (int j = 0; j < 10; j++)
+//     {
+//         Console.WriteLine("The value of j is: " + j);
+//     }
+//     Console.WriteLine("Loop finished! The final value of j is: 10")
+//     Console.ReadKey();
+// }
+
+// static void Main(string[] args)
+// {
+//     Console.WriteLine("Enter the number of rows: ");
+//     int rows = Convert.ToInt32(Console.ReadLine())
+//     Console.WriteLine("Enter the number of columns: ");
+//     int columns = Convert.ToInt32(Console.ReadLine())
+//     Console.WriteLine("What symbol do you want to use? ");
+//     string symbol = Console.ReadLine()
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             Console.Write(symbol);
+//         }
+//         Console.WriteLine(); // Move to the next line after printing a row
+//     }
+
 namespace MyFirstProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-
-            int num = random.Next(1, 7);
-
-            Console.WriteLine("Rolling a die...");
-            Console.WriteLine("You rolled a " + num + "!");
-
-
-
-
-            Console.ReadKey();
+            
         }
     }
 }
