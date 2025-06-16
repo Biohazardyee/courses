8851cb5856aff36e3c234aa1e29fba06a3824b45
Marlier Thibaud <thibaud.marlier@algosup.com>
2025-06-16 12:10:38 +0200

    ok

--- Year 2/Computer Science/C# /MoonshotTestApp/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MoonshotTestApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
--- Year 2/Computer Science/C# /ToDoListApp/obj/Debug/net9.0/ToDoListApp.AssemblyInfo.cs
+++ Year 2/Computer Science/C# /ToDoListApp/obj/Debug/net9.0/ToDoListApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+156d37e8fff2d1a671cbdab13365509d14dc0752")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7148fab65cbdc46d2b6418c3b2ceaf95b8a9ac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
